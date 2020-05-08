f90687687d7b66846b920c1ef95b2d04c7a44601
OK <hafiznur312@gmail.com>
2020-05-08 17:50:59 +0700

    Modul 11

---
+++ Modul 11/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ2rftGk9EOHaflABkNBZ0VDFSBucqRPA",
+    authDomain: "pbfm11.firebaseapp.com",
+    databaseURL: "https://pbfm11.firebaseio.com",
+    projectId: "pbfm11",
+    storageBucket: "pbfm11.appspot.com",
+    messagingSenderId: "260787752569",
+    appId: "1:260787752569:web:bcd3161aebe8e55eab8ea8",
+    measurementId: "G-4CCGC0LX9P"
+  };
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;

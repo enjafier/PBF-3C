bdb8dfe0168f71f447d8a985150868a8f27a0c9e
OK <hafiznur312@gmail.com>
2020-05-08 17:51:56 +0700

    Modul 12

---
+++ Modul 12/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ2rftGk9EOHaflABkNBZ0VDFSBucqRPA",
+  authDomain: "pbfm11.firebaseapp.com",
+  databaseURL: "https://pbfm11.firebaseio.com",
+  projectId: "pbfm11",
+  storageBucket: "pbfm11.appspot.com",
+  messagingSenderId: "260787752569",
+  appId: "1:260787752569:web:bcd3161aebe8e55eab8ea8",
+  measurementId: "G-4CCGC0LX9P"
+};
+export default firebaseConfig;
